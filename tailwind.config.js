ac0aa28b57bb02f7653acad64a4c42db462f54df
mehedi66 <mh5203280@gmail.com>
2025-08-29 16:00:35 +0600

    simple edting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,1 @@
 import "tailwindcss";
-
-
-
-//   <script>
-//     let count = 0;
-//     let navHeart = document.getElementById("heart-btn");
-
-//     document.querySelectorAll("#heart-icon").forEach(h => {
-//       h.onclick = () => {
-//         count++;
-//         navHeart.textContent = count;
-//       };
-//     });
-//    </script>
